afeccd6c2b69d7c639c91dff43ab98451fb7ffa7
LucianoMariotti <luciano.mariotti99@gmail.com>
2021-06-08 17:58:56 -0300

    first commit

--- js/programa_3.js
+++ js/programa_3.js
@@ -2,23 +2,47 @@
 
 function comprobarpassword(){
 
-    let password = document.getElementById('contraseña').value;  
-    
+    let password = document.getElementById('contraseña').value;
+
     let espacios = comprobarespacios(password); //true TIENE espacios, false SI NO TIENE espacios
+    
+    let signos = comprobarsignos(password);  //lo mismo con los signos (1 o mas)
+    
+    let cantidadcaracteres = password.length; //entre 8 y 15 inclusive
 
-    let signos = comprobarsignos(password);  //lo mismo con los signos
+    let cantidadmayusculas = comprobarmayusculas(password); //2 mayusculas o mas
 
-    let cantidadcaracteres = password.length;
+    let numerosunicos = comprobarnumerosrepetidos(password); //3 numeros unicos o mas
 
-    let cantidadmayusculas = comprobarmayusculas(password);
 
-    let numerosunicos = comprobarnumerosrepetidos(password);
+    if ((!signos && !espacios) && (cantidadmayusculas > 1) && (numerosunicos > 2) && (cantidadcaracteres > 7) && (cantidadcaracteres < 16)) {
+        alert("La contraseña es valida")
+    }
 
+    if(espacios){
+        alert("contiene espacios")
+    }
+    
+    if (signos) {
+        alert("debe tener al menos un signo")
+    }
+
+    if (cantidadcaracteres < 8 || cantidadcaracteres>15) {
+        alert("la contraseña debe tener entre 8 y 15 caracteres")
+    }
+
+    if (cantidadmayusculas < 2) {
+        alert("la contraseña debe tener al menos 2 letras mayusculas")
+    }
+    
+    if (numerosunicos < 3) {
+        alert("la contraseña debe tener al menos 3 numeros no repetidos")
+    }
 }
 
 function comprobarespacios(pw){
     
-    let password2 = pw.replace(/[^\w\s!?]/g,'');                    //quito los espacios
+    let password2 = pw.replace(/ /g, "")                    //quito los espacios
 
     if (pw != password2) { //comparo la misma password con y sin espacios
         return true
@@ -29,14 +53,14 @@ function comprobarespacios(pw){
 }
 
 function comprobarsignos(pw){
-    
+
     let password2 = pw.replace(/[^a-zA-Z0-9]/g, ''); //elimino los signos con expresion regular
 
     if (pw != password2) {      //comparo la misma password con o sin signos
-        return true            //si son distintas, quiere decir que tiene al menos un signo, devuelve true
+        return false            
     }
     else{ 
-        return false
+        return true
     }
 }
 
@@ -70,16 +94,12 @@ function comprobarnumerosrepetidos(pw){ //en esta funcion ordeno la contraseña 
         return 0;
       })
 
-    console.log(arraypassword);
-
     for(let i = 0; i < arraypassword.length; i++){
-        if( !isNaN(arraypassword[i]) && arraypassword[i] == arraypassword[i+1] ){ //si el caracter es un numero y el proximo en el array es diferente (osea es un numero unico) suma el contador
+        if( !isNaN(arraypassword[i]) && (arraypassword[i] != arraypassword[i+1] && arraypassword[i] != arraypassword[i-1]) ){ //si el caracter es un numero unico, ya que el siguiente y anterior en el arreglo son diferentes (acuerdese que estan ordenados) se suma el contador
             numeros++;
         }
     }
-    console.log(numeros)
-    //cambios!!!
-//numeros se vuelve .lengt y voy restando, === mismo tipo e igual valor 
+    return numeros;
 }
 
 document.querySelector("#boton3").addEventListener("click",comprobarpassword);
